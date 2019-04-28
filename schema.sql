0753ae617314f98cb7d057815f5eb8bf197640b8
Володя <sizanv@gmail.com>
2019-04-28 15:15:49 +0300

    внес корректировки согласно комментарию наставника

--- schema.sql
+++ schema.sql
@@ -5,46 +5,46 @@ default collate utf8_general_ci;
 use YetiCave;
 
 create table categories (
-cat_name nvarchar(30),
-symbol_code nvarchar(30),
-cat_id int auto_increment primary key);
-
-create table lot (
-date_create timestamp,
-lot_name nvarchar(200),
-lot_description nvarchar(4000),
-lot_img nvarchar(4000),
-start_price int,
-date_end timestamp,
-lot_rate_step decimal,
-lot_id int auto_increment primary key);
-
-alter table lot
-add column 
-	(autor_id int not null,
-	winner_id int,
-    category_id int not null);    
+id int auto_increment primary key,
+name char(255) not null,
+symbol_code char(255) not null);
 
 create table rate (
-rate_date timestamp,
-suplied_price int,
-user_id int,
-lot_id int,
-rate_id int auto_increment primary key);
+id int auto_increment primary key,
+r_date timestamp not null,
+suplied_price int not null,
+user_id int not null,
+lot_id int not null,
+foreign key (user_id) references user(id),
+foreign key (lot_id) references lot(id));
 
-create table user_description (
-registration_date timestamp,
-email nvarchar(200) unique,
-user_name nvarchar(40) unique,
-user_password nvarchar(50),
-user_avatar nvarchar(4000),
-contact_inf nvarchar(4000),
-user_id int auto_increment primary key,
-created_lot_id int,
-created_rate_id int);
+create table user (
+id int auto_increment primary key,
+registration_date timestamp not null,
+email char(255) unique not null,
+name char(255) unique not null,
+password char(255) not null,
+avatar text null,
+contact_inf text null);
+
+create table lot (
+id int auto_increment primary key,
+date_create timestamp not null,
+name char(255) not null,
+description text,
+img text,
+start_price int,
+date_end timestamp null,
+rate_step decimal,
+author_id int not null,
+winner_id int null,
+category_id int not null,
+foreign key (author_id) references user(id),
+foreign key (winner_id) references user(id),
+foreign key (category_id) references categories(id));  
 
 insert into categories 
-(cat_name, symbol_code) values 
+(name, symbol_code) values 
 ('Доски и лыжи','boards'), 
 ('Крепления','attachment'), 
 ('Ботинки', 'boots'), 
@@ -52,10 +52,12 @@ insert into categories
 ('Инструменты','tools'), 
 ('Разное','other');
 
-create index cat_int on lot(category_id);
-create index w_int on lot(winner_id);
-create index aut_int on lot(autor_id);
-create index u_int on rate(user_id);
-create index l_int on rate(lot_id);
-create index crl_int on user_description(created_lot_id);
-create index crra_int on user_description(created_rate_id);
+create index categor_ind on lot(category_id);
+create index winner_ind on lot(winner_id);
+create index author_ind on lot(author_id);
+create index user_ind on rate(user_id);
+create index lot_ind on rate(lot_id);
+create index categor_prim_ind on categories(id);
+create index lot_prim_ind on lot(id);
+create index rate_prim_ind on rate(id);
+create index user_prim_ind on user(id);
