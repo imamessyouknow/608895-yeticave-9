780018de2c3fe965f642d139ab4df6751a931289
Володя <sizanv@gmail.com>
2019-05-06 20:14:50 +0300

    Подключил БД к проекту

--- functions-data/data.php
+++ functions-data/data.php
@@ -1,17 +1,28 @@
 <?php
+
+$con = mysqli_connect("localhost", "root", "root","YetiCave");
+
+if ($con == false) {
+	print("Ошибка подключения к БД: " . mysqli_connect_error());
+}
+mysqli_options($con, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+mysqli_set_charset($con, "utf8");
+
+//получаем список категорий и их символьных имен 
+$categories_sql = "select name, symbol_code from categories;";
+$cat_result = mysqli_query($con,$categories_sql);
+$categories = mysqli_fetch_all($cat_result, MYSQLI_ASSOC);
+
+//получаем список лотов, открытых за последние 5 дней 
+$lots_sql = "select l.name as lot_name, start_price, date_create, img, current_price, c.name as cat_name
+			from lot l join categories c on l.category_id = c.id
+			where date_create >= adddate(now(), interval - 5 day);";
+$lot_result = mysqli_query($con, $lots_sql);
+$suply = mysqli_fetch_all($lot_result, MYSQLI_ASSOC);
+
+
 $is_auth = rand(0, 1);
 $user_name = 'Владимир Сизанюк'; // укажите здесь ваше имя
 $timezone = 'Europe/Moscow';
 $locale = 'ru_RU';
-
-$categories = ['Доски и лыжи','Крепления','Ботинки','Одежда','Инструменты','Разное'];
-$suply = [
-	['name' => '2014 Rossignol District Snowboard', 'category' => $categories[0], 'price' => 10999, 'url' => 'img/lot-1.jpg'],
-	['name' => 'DC Ply Mens 2016/2017 Snowboard', 'category' => $categories[0], 'price' => 159999, 'url' => 'img/lot-2.jpg'],
-	['name' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'category' => $categories[1], 'price' => 8000, 'url' => 'img/lot-3.jpg'],
-	['name' => 'Ботинки для сноуборда DC Mutiny Charocal', 'category' => $categories[2], 'price' => 10999, 'url' => 'img/lot-4.jpg'],
-	['name' => 'Куртка для сноуборда DC Mutiny Charocal', 'category' => $categories[3], 'price' => 7500, 'url' => 'img/lot-5.jpg'],
-	['name' => 'Маска Oakley Canopy', 'category' => $categories[5], 'price' => 5400, 'url' => 'img/lot-6.jpg']
-];
-$objects_names = ['name', 'category', 'price', 'url'];
 ?>
