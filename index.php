527a0610d1fa178da1b10ae75635460d4bb0e3d3
Володя <sizanv@gmail.com>
2019-05-11 13:48:55 +0300

    Отформатировал по PSR-2

--- index.php
+++ index.php
@@ -10,10 +10,26 @@ if (date_interval_format($interval,'%H') < 1) {
 	$time_left_class = 'lot__timer timer timer--finishing';
 }
 
-$page_content = include_template('index.php', ['categories' => $categories, 'suply' => $suply, 'diff_format' => $diff_format, 'time_left_class' => $time_left_class]);
-$layout_content = include_template('layout.php', ['content' => $page_content, 'title' => 'YetiCave - Главная', 'categories' => $categories, 'is_auth' => $is_auth, 'user_name' => $user_name]);
+$page_content = include_template('index.php', 
+	[
+	 'categories' => $categories, 
+	 'suply' => $suply, 
+	 'diff_format' => $diff_format,
+	 'time_left_class' => $time_left_class
+	]
+);
+
+$layout_content = include_template('layout.php', 
+	[
+	 'content' => $page_content,
+	 'title' => 'YetiCave - Главная',
+	 'categories' => $categories,
+	 'is_auth' => $is_auth,
+	 'user_name' => $user_name
+ 	]
+);
 
 print($layout_content);
-?>
+
 
 
