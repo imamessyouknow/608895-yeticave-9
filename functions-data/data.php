696ddbf4b1a80a5151551ac4475056c7ed8438e3
Володя <sizanv@gmail.com>
2019-06-09 18:22:39 +0300

    правки

--- functions-data/data.php
+++ functions-data/data.php
@@ -1,38 +1,47 @@
 <?php
 
-$is_auth = rand(0, 1);
+$is_auth = rand (0, 1);
 $user_name = 'Владимир Сизанюк'; // укажите здесь ваше имя
 $timezone = 'Europe/Moscow';
 $locale = 'ru_RU';
 
-date_default_timezone_set($timezone);
-setlocale(LC_ALL, $locale);
+date_default_timezone_set ($timezone);
+setlocale (LC_ALL, $locale);
 
 //подсчет дат и интервалов
-$curdate = date_create('now');
-$tummdate = date_create('tomorrow');
-$interval = date_diff($curdate, $tummdate);
-$diff_format = date_interval_format($interval, '%H:%I');
+$curdate = date_create ('now');
+$tummdate = date_create ('tomorrow');
+$interval = date_diff ($curdate, $tummdate);
+$diff_format = date_interval_format ($interval, '%H:%I');
 
 //подключение к БД
-$con = mysqli_connect("localhost", "root", "root","YetiCave");
+$con = mysqli_connect ("localhost", "root", "root", "YetiCave");
 
-if ($con == false) {
-	print("Ошибка подключения к БД: " . mysqli_connect_error());
+if ( $con == false ) {
+    print( "Ошибка подключения к БД: " . mysqli_connect_error () );
 }
-mysqli_options($con, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
-mysqli_set_charset($con, "utf8");
+mysqli_options ($con, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+mysqli_set_charset ($con, "utf8");
 
-//получаем список категорий и их символьных имен 
-$categories_sql = "select name, symbol_code from categories;";
-$cat_result = mysqli_query($con,$categories_sql);
-$categories = mysqli_fetch_all($cat_result, MYSQLI_ASSOC);
+//получаем список категорий и их символьных имен
+$categories_sql =
+    "select name, 
+            symbol_code 
+     from categories;";
 
-//получаем список лотов, открытых за последние  N дней 
-$lots_sql = "select l.name as lot_name, l.id, start_price, date_create, img, current_price, c.name as cat_name
-			from lot l join categories c on l.category_id = c.id
-			where date_create >= adddate(now(), interval - 30 day);";
-$lot_result = mysqli_query($con, $lots_sql);
-$suply = mysqli_fetch_all($lot_result, MYSQLI_ASSOC);
+$cat_result = mysqli_query ($con, $categories_sql);
+$categories = mysqli_fetch_all ($cat_result, MYSQLI_ASSOC);
 
-?>
+//получаем список лотов, открытых за последние  N дней
+$lots_sql =
+    'select l.name as lot_name, 
+            l.id, start_price, 
+            date_create, 
+            img, 
+            current_price, 
+            c.name as cat_name
+	 from lot l join categories c on l.category_id = c.id
+	 where date_create >= adddate(now(), interval - 30 day);';
+
+$lot_result = mysqli_query ($con, $lots_sql);
+$suply = mysqli_fetch_all ($lot_result, MYSQLI_ASSOC);
